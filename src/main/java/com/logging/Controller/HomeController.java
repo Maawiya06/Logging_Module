8b1daa004da015c095c1bb068c30acd76e5c4726
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-29 22:38:40 +0530

    commit

--- src/main/java/com/logging/Controller/HomeController.java
+++ src/main/java/com/logging/Controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.logging.Controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
     @GetMapping
     public String home(){
+        logger.info("this is private API");
+        logger.debug("This is debug");
         return "This is my controller";
     }
 }
