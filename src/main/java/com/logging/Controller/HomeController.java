a316716de5d7f8b3cf9b4191c91dac1aa0c7d7b0
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-30 13:19:46 +0530

    commit

--- src/main/java/com/logging/Controller/HomeController.java
+++ src/main/java/com/logging/Controller/HomeController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -15,12 +16,12 @@ public class HomeController {
 
     private Logger logger = LoggerFactory.getLogger(HomeController.class);
     @GetMapping
-    public String home(){
-        logger.error("this is private API");
-        logger.warn("this is private API");
-        logger.info("this is private API");
-        logger.debug("this is private API");
-        logger.trace("this is private API");
+    public String home(@RequestParam(defaultValue = "Default Value", required = false) String value){
+        logger.error("this is private API {}", value);
+        logger.warn("this is private API {}", value);
+        logger.info("this is private API {}", value);
+        logger.debug("this is private API {}", value);
+        logger.trace("this is private API {}", value);
 
         return "This is my controller";
     }
